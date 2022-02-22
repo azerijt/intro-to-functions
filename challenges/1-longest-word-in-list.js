cb0524257806fc6bcdc440dd3a3c304c6973dce6
Anna-Zeri Temir <azjuliantemir@gmail.com>
2022-02-22 18:53:37 +0000

    completed longest-word

--- challenges/1-longest-word-in-list.js
+++ challenges/1-longest-word-in-list.js
@@ -1,31 +1,18 @@
-Write a function that, given a list of strings, returns the string which has the greatest character count (including duplicates).
+console.log(longestInList(["England", "Wales", "Scotland", "Northern Ireland"])) //should be "Northern Ireland"
+console.log(longestInList(["the", "quick", "brown", "fox"])) //should be "quick"
+console.log(longestInList(["hello", "hi", "greetings", "hey"])) //should be "greetings"
 
-When there are multiple string which are tied for largest character count, return the string that occurs earliest in the list.
-
-You may assume that each string in the list is a single word.
-You may assume that there are always at least two strings in the list.
-
-function:
-=========
-name: longestInList
-
-parameters:
-	- WORDS: list of strings
-
-return type: string
-
-pseudo code:
-============
-set LONGEST to the 0th element in WORDS
-
-for each string, WORD, in WORDS
-	if WORD has more characters than LONGEST
-		set LONGEST to WORD
-
-return LONGEST
-
-test cases:
-===========
-longestInList(["England", "Wales", "Scotland", "Northern Ireland"]) should be "Northern Ireland"
-longestInList(["the", "quick", "brown", "fox"]) should be "quick"
-longestInList(["hello", "hi", "greetings", "hey"]) should be "greetings"
+function longestInList(words){
+	let longest = words[0]
+	
+	for (let word = 0; word < words.length; word++) {
+		if (words[word].length > longest.length){
+			longest = words[word];
+			
+		}
+		
+	
+	}
+	
+return longest
+}
